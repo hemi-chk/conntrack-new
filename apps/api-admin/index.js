302853c52d3741396e3371c1e15404a955ec1db0
Hemindi <kalubovilakch.23@uom.lk>
2026-06-23 02:01:27 +0530

    Network Response issue managed

--- apps/api-admin/index.js
+++ apps/api-admin/index.js
@@ -9,6 +9,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5002
 
+app.use(cors())
 app.use(express.json())
 
 // Apply auth middleware to all admin routes, requiring 'admin' role
